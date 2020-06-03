089ac8ce8dd49afd327b1ce1c214180bf144317c
Peter Taylor <PTaylor@FluenTech.info>
2020-06-02 19:00:41 -0600

    - fix crate example in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@
 //! assert!(instant1 < instant2);    // instant1 is *before* instant2
 //!
 //! // duration is the difference between the instances
-//! let duration: Option<Microseconds<i64>> = instant2.duration_since(&instant1);    
+//! let duration: Option<Microseconds<i64>> = instant2.elapsed_since(&instant1);    
 //!
 //! assert!(duration.is_some());
 //! assert_eq!(instant1 + duration.unwrap(), instant2);
