40e058dd4030ebd37bb073c7b0ad76b990c50807
Peter Taylor <PTaylor@FluenTech.info>
2020-07-18 17:55:02 -0600

    test(Duration, Rate): Add benchmark for comparing Duration and Rate operations
    Not generally useful unless making a significant change. One can be
    changed and the other left as-is for clear comparison.

---
+++ benches/duration_vs_rate.rs
@@ -0,0 +1,33 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use embedded_time::{traits::*, units::*};
+
+fn rem_duration_rate(c: &mut Criterion) {
+    use core::time::Duration;
+
+    let mut group = c.benchmark_group("Rem Duration vs Rate");
+
+    group.bench_function("duration", |b| {
+        b.iter(|| {
+            let dur1 = black_box(402);
+            let dur2 = black_box(10);
+            let rem = Seconds::<u64>(dur1) % Seconds::<u64>(dur2);
+            let count = rem.count();
+            let _ = black_box(count);
+        })
+    });
+
+    group.bench_function("rate", |b| {
+        b.iter(|| {
+            let rate1 = black_box(402);
+            let rate2 = black_box(10);
+            let rem = Hertz::<u64>(rate1) % Hertz::<u64>(rate2);
+            let count = rem.count();
+            let _ = black_box(count);
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, rem_duration_rate);
+criterion_main!(benches);
