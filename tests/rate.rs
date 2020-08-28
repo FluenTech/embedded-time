9633d2d2fb6c5f8c32e602e2db17b43c7ea8d97c
Peter Taylor <PTaylor@FluenTech.info>
2020-08-28 17:14:21 -0600

    feat(rate): Add more arithmetic
    - multiplication by integer
    - checked multiplication by integer
    - division by integer
    - checked division by integer

--- tests/rate.rs
+++ tests/rate.rs
@@ -79,6 +79,45 @@ fn sub() {
 }
 
 #[test]
+fn mul() {
+    assert_eq!((Kilohertz(2_001_u32) * 2), Kilohertz(4_002_u32));
+}
+#[test]
+#[should_panic]
+fn mul_overflow() {
+    let _ = Kilohertz(u32::MAX) * 2;
+}
+#[test]
+fn checked_mul() {
+    assert_eq!(
+        Kilohertz(2_001_u32).checked_mul(&2),
+        Some(Kilohertz(4_002_u32))
+    );
+
+    assert_eq!(Kilohertz(u32::MAX).checked_mul(&2), None);
+}
+
+#[test]
+fn div() {
+    assert_eq!((Kilohertz(2_002_u32) / 2), Kilohertz(1_001_u32));
+}
+#[test]
+#[should_panic]
+fn div_div_by_zero() {
+    let _ = Milliseconds(u32::MAX) / 0;
+}
+
+#[test]
+fn checked_div() {
+    assert_eq!(
+        Milliseconds(2_002_u32).checked_div(&2),
+        Some(Milliseconds(1_001_u32))
+    );
+
+    assert_eq!(Milliseconds(u32::MAX).checked_div(&0), None);
+}
+
+#[test]
 fn rem() {
     assert_eq!(100_u32.bps() % u64::MAX.MiBps(), 100_u32.bps());
     assert_eq!(10_020_u32.Bps() % 1_u64.kBps(), 20_u32.Bps());
